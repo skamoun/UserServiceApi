f73a58418216420a0b6d3efeda1d1ced3bdc59b0
kamoun said <said.kamoun@gmail.com>
2022-11-24 13:29:08 +0100

    add security config to overide spring security & bean to save init data

--- src/main/java/ma/iss/userservice/UserserviceApplication.java
+++ src/main/java/ma/iss/userservice/UserserviceApplication.java
@@ -1,7 +1,14 @@
 package ma.iss.userservice;
 
+import ma.iss.userservice.domain.AppUser;
+import ma.iss.userservice.domain.Role;
+import ma.iss.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
@@ -9,5 +16,27 @@ public class UserserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
+	@Bean
+ CommandLineRunner run (UserService userService){
+		return  args -> {
+			userService.saveRole(new Role(null,"ROLE_USER"));
+			userService.saveRole(new Role(null,"ROLE_MANAGER"));
+
+			userService.saveRole(new Role(null,"ROLE_ADMIN"));
+
+			userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
+			userService.saveUser(new AppUser(null,"said kmn","skamoun","1234",new ArrayList<>()));
+			userService.saveUser(new AppUser(null,"amine kmn","akamoun","12345",new ArrayList<>()));
+			userService.saveUser(new AppUser(null,"ahmed kmn","ahkamoun","123456",new ArrayList<>()));
+			userService.saveUser(new AppUser(null,"karim kmn","krkamoun","1234567",new ArrayList<>()));
+			userService.addRoleToUser("skamoun","ROLE_USER");
+			userService.addRoleToUser("skamoun","ROLE_MANAGER");
+			userService.addRoleToUser("skamoun","ROLE_ADMIN");
+			userService.addRoleToUser("akamoun","ROLE_MANAGER");
+			userService.addRoleToUser("ahkamoun","ROLE_ADMIN");
+			userService.addRoleToUser("krkamoun","ROLE_SUPER_ADMIN");
+
+		};
+ }
 
 }
--- src/main/java/ma/iss/userservice/api/UserRessource.java
+++ src/main/java/ma/iss/userservice/api/UserRessource.java
@@ -1,13 +1,15 @@
 package ma.iss.userservice.api;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import ma.iss.userservice.domain.AppUser;
+import ma.iss.userservice.domain.Role;
 import ma.iss.userservice.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,4 +21,28 @@ public class UserRessource {
     public ResponseEntity<List<AppUser>> getUsers(){
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<AppUser> saveUser(@RequestBody AppUser user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveUser(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> saveUser(@RequestBody RoleToUserForm roleToUserForm){
+        userService.addRoleToUser(roleToUserForm.getUsername(),roleToUserForm.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+
+}
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
