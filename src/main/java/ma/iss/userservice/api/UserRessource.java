bf4b47644a65e6c9e808ef87c5cac79d3cf89677
kamoun said <said.kamoun@gmail.com>
2022-11-27 00:07:06 +0100

    handling token expiring

--- src/main/java/ma/iss/userservice/api/UserRessource.java
+++ src/main/java/ma/iss/userservice/api/UserRessource.java
@@ -1,20 +1,40 @@
 package ma.iss.userservice.api;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ma.iss.userservice.domain.AppUser;
 import ma.iss.userservice.domain.Role;
 import ma.iss.userservice.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
-import java.util.List;
+import java.util.*;
+
+import static java.util.Arrays.stream;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Slf4j
 public class UserRessource {
     private  final UserService userService;
      @GetMapping("/users")
@@ -38,7 +58,45 @@ public class UserRessource {
         userService.addRoleToUser(roleToUserForm.getUsername(),roleToUserForm.getRoleName());
         return ResponseEntity.ok().build();
     }
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        final String bearer = "Bearer";
+        if (authorizationHeader != null && authorizationHeader.startsWith(bearer)) {
+            try {
+                String refresh_token = authorizationHeader.substring(bearer.length()); // remove bearer from header
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refresh_token);
+                String username = decodedJWT.getSubject();
+                AppUser user = userService.getUser(username);
+                String access_token = JWT.create()
+                        .withSubject(user.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 10*60*1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles",user.getRoles().stream().map(Role::getName).toList())
+                        .sign(algorithm);
+                Map<String,String> tokens = new HashMap<>();
+                tokens.put("access_token",access_token);
+                tokens.put("refresh_token",refresh_token);
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(),tokens);
 
+            } catch (JWTVerificationException e) {
+                log.error("Error logging : {}", e.getMessage());
+                response.setHeader("error", e.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                Map<String, String> errors = new HashMap<>();
+                errors.put("error_msg", e.getMessage());
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), errors);
+            }
+
+        } else {
+           throw new RuntimeException(" no referesh token sended");
+
+        }
+    }
 
 }
 @Data
